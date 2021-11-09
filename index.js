16a56d76fe53582171078bfb23d64d002e18e277
M-ister-IO <94001828+M-ister-IO@users.noreply.github.com>
2021-11-09 18:39:13 +0100

    1.03
    new gifs

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ var textArray = [
     'Tu bois',
     'https://tenor.com/view/why-dont-you-go-get-me-a-drink-zoe-levin-tiffany-chester-bonding-i-dare-you-gif-16541342',
     'https://tenor.com/view/oss117-oss-le-caire-nid-despions-fran%C3%A7ois-damiens-drink-gif-16938746',
-    'https://tenor.com/view/alcool-belgrade-ptit-sifflet-fanta-gif-16824147'
+    'https://tenor.com/view/alcool-belgrade-ptit-sifflet-fanta-gif-16824147',
+    'https://tenor.com/view/beer-german-happy-hour-gif-4728652',
+    'https://tenor.com/view/leonardo-dicaprio-drink-and-wink-wink-and-drink-cheers-gif-13397942',
+    'https://tenor.com/view/liquor-oldman-gif-18480181',
+    'https://tenor.com/view/drinking-cheers-old-drunk-gif-4497075'
 ];
 var textArray2=[
     "ducoup",
